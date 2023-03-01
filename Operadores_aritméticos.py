f314f22108008fd18e41f4809a41ca8e66f0f3a8
Sergio_Martinez <sergiomartinez121unisangil.edu.co>
2023-02-28 23:16:26 -0500

    Cambio1

---
+++ Operadores_aritméticos.py
@@ -0,0 +1,22 @@
+#Liberia
+import math
+##Adquirir información
+a = int(input("Dijite el numero 1: "))
+print(f"El numero es: {a}")
+b = int(input("Dijite el numero 1: "))
+print(f"El numero es: {b}")
+#proceso
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a ** b
+h = math.sqrt(a)
+#salida
+print(f"El resultado de la suma es: {c}")
+print(f"El resultado de la resta es: {d}")
+
+print(f"El resultado de la division es: {f}")
+print(f"El resultado de la potencia es: {g}")
+print(f"El resultado de la raiz es: {h}")
+print("cambio git hub")
